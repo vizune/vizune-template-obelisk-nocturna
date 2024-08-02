9c7d771f084f261093b04985542670f2811f42af
Sasha Richardson <sasha_richardson@hotmail.co.uk>
2024-08-02 11:39:36 +0100

    Content corner decorations with nuxtimg

--- components/Content.vue
+++ components/Content.vue
@@ -1,13 +1,19 @@
 <template>
     <main class="Content container mx-auto pb-[3.5rem]">
         <div class="bg-white p-1 mx-4">
-            <div class="Corners-top"></div>
+            <div class="Corners-top">
+                <NuxtImg format="webp" width="150" height="150" src="/floral-corner.png" />
+                <NuxtImg format="webp" width="150" height="150" src="/floral-corner.png" />
+            </div>
             <div class="border-4 border-midnight flex flex-col justify-between py-[4rem] md:py-6 md:pb-[8rem]">
                 <div class="px-6 md:px-[4rem] xl:px-[8rem] py-6 grow-1">
                     <slot />
                 </div> 
             </div>
-            <div class="Corners-bottom"></div>
+            <div class="Corners-bottom">
+                <NuxtImg format="webp" width="150" height="150" src="/floral-corner.png" />
+                <NuxtImg format="webp" width="150" height="150" src="/floral-corner.png" />
+            </div>
         </div>
     </main>
 </template>
@@ -21,26 +27,21 @@ $corner-offset: 2px;
         position: relative;
         width: 100%;
 
-        &:before {
-            content: '';
+        > *:first-child {
             position: absolute;
             top: $corner-offset;
             left: $corner-offset;
-            background: url('/floral-corner.png') no-repeat;
             height: $corner-size;
             width: $corner-size;
-            background-size: 100% 100%;
         }
 
-        &:after {
+        > *:last-child {
             content: '';
             position: absolute;
             top: $corner-offset;
             right: $corner-offset;
-            background: url('/floral-corner.png') no-repeat;
             height: $corner-size;
             width: $corner-size;
-            background-size: 100% 100%;
             transform: rotate(90deg);
         }
     }
@@ -49,27 +50,21 @@ $corner-offset: 2px;
         position: relative;
         width: 100%;
 
-        &:before {
-            content: '';
+        > *:first-child {
             position: absolute;
             bottom: $corner-offset;
             left: $corner-offset;
-            background: url('/floral-corner.png') no-repeat;
             height: $corner-size;
             width: $corner-size;
-            background-size: 100% 100%;
             transform: rotate(-90deg);
         }
 
-        &:after {
-            content: '';
+        > *:last-child {
             position: absolute;
             bottom: $corner-offset;
             right: $corner-offset;
-            background: url('/floral-corner.png') no-repeat;
             height: $corner-size;
             width: $corner-size;
-            background-size: 100% 100%;
             transform: rotate(180deg);
         }
     }
